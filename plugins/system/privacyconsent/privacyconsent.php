cae61c27d3d2f549843c56fafdc0cb2e3cf96040
Nicola Galgano <extensions4joomla@gmail.com>
2018-06-20 19:19:57 +0200

    remindExpiringConsents()
    Method to send the remind for privacy consents renew

--- plugins/system/privacyconsent/privacyconsent.php
+++ plugins/system/privacyconsent/privacyconsent.php
@@ -486,6 +486,110 @@ class PlgSystemPrivacyconsent extends JPlugin
 	}
 
 	/**
+	 * Method to send the remind for privacy consents renew
+	 *
+	 * @return  int
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	private function remindExpiringConsents()
+	{
+		// Load the parameters.
+		$expire = (int) $this->params->get('consentexpiration', 365);
+		$remind = (int) $this->params->get('remind', 30);
+		$now    = JFactory::getDate()->toSql();
+		$period = '-' . ($expire - $remind);
+
+		$db    = $this->db;
+		$query = $db->getQuery(true)
+			->select('r.id, r.user_id, u.email');
+		$query->from($db->quoteName('#__privacy_consents', 'r'));
+		$query->join('LEFT', $db->quoteName('#__users', 'u') . ' ON u.id = r.user_id');
+		$query->where($query->dateAdd($now, $period, 'DAY') . ' > ' . $db->quoteName('created'));
+		$query->where($db->quoteName('remind') . ' = 0');
+
+		try
+		{
+			$users = (int) $db->setQuery($query)->loadObjectList();
+		}
+		catch (JDatabaseException $exception)
+		{
+			return false;
+		}
+
+		foreach ($users as $user)
+		{
+			$token       = JApplicationHelper::getHash(JUserHelper::genRandomPassword());
+			$hashedToken = JUserHelper::hashPassword($token);
+
+			// The mail
+			try
+			{
+				$app = JFactory::getApplication();
+
+				$linkMode = $app->get('force_ssl', 0) == 2 ? 1 : -1;
+
+				$substitutions = array(
+					'[SITENAME]' => $app->get('sitename'),
+					'[URL]'      => JUri::root(),
+					'[TOKENURL]' => JRoute::link('site', 'index.php?option=com_privacy&view=extend&confirm_token=' . $token, false, $linkMode),
+					'[FORMURL]'  => JRoute::link('site', 'index.php?option=com_privacy&view=extend', false, $linkMode),
+					'[TOKEN]'    => $token,
+					'\\n'        => "\n",
+				);
+
+				$emailSubject = JText::_('PLG_SYSTEM_PRIVACYCONSENT_EMAIL_REMIND_SUBJECT');
+				$emailBody = JText::_('PLG_SYSTEM_PRIVACYCONSENT_EMAIL_REMIND_BODY');
+
+				foreach ($substitutions as $k => $v)
+				{
+					$emailSubject = str_replace($k, $v, $emailSubject);
+					$emailBody    = str_replace($k, $v, $emailBody);
+				}
+
+				$mailer = JFactory::getMailer();
+				$mailer->setSubject($emailSubject);
+				$mailer->setBody($emailBody);
+				$mailer->addRecipient($user->email);
+
+				$mailResult = $mailer->Send();
+
+				if ($mailResult instanceof JException)
+				{
+					return false;
+				}
+				elseif ($mailResult === false)
+				{
+					return false;
+				}
+
+				// Update the privacy_consents item for not send the reminder again
+				$query->clear()
+					->update($db->quoteName('#__privacy_consents'))
+					->set($db->quoteName('remind') . ' = 1 ')
+					->set($db->quoteName('token') . ' = ' . $db->quote($hashedToken))
+					->where($db->quoteName('id') . ' = ' . $db->quote($user->id));
+				$db->setQuery($query);
+
+				try
+				{
+					$db->execute();
+				}
+				catch (RuntimeException $e)
+				{
+					return false;
+				}
+
+				return true;
+			}
+			catch (phpmailerException $exception)
+			{
+				return false;
+			}
+		}
+	}
+
+	/**
 	 * Method to delete the expired privacy consents
 	 *
 	 * @return  int
